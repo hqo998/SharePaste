79dc78e33e7fea54762e660ead430881d0e0db5c
Charles <charlesvonkalm@gmail.com>
2025-12-03 12:41:20 +1100

    Successfully serves a basic html page to front.
    Using c++23 print statements

--- src/backend/main.cpp
+++ src/backend/main.cpp
@@ -1,30 +1,50 @@
 #include <iostream>
+#include <string>
+#include <filesystem>
+#include <print>
+
 #include <sqlite3.h>
 #include <httplib.h>
+
 
 void getRequestAPI(const httplib::Request &, httplib::Response &res)
 {
     res.set_content("Hello world!", "text/plain");
 }
 
-void serveFrontEnd()
+void serveFrontEnd(const httplib::Request &, httplib::Response &res)
 {
-    
+    res.set_file_content("./www/index.html");
+}
+
+void checkMissingFrontend()
+{
+    std::println("[TEST] Looking for web directory");
+
+    const std::string webroot = "./www";
+
+    if (!std::filesystem::exists(webroot)) 
+    std::println("[WARNING] Web directory not found");
+    else
+    std::println("[PASS] Found web directory");
+}
+
+void checkMissingFiles()
+{
+    checkMissingFrontend();
 }
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    
+    std::println("[START] Beginning Sharenote");
+
     httplib::Server svr;
+
+    checkMissingFiles();
 
     svr.Get("/api", getRequestAPI);
 
-    auto ret = svr.set_mount_point("/", "./www");
-    if (!ret)
-    {
-        std::cerr << "Frontend folder not found!" << '\n';
-    }
+    svr.Get("/", serveFrontEnd);
 
     svr.listen("0.0.0.0", 80);
 }
